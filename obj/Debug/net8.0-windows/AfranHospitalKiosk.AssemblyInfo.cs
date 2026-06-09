ea357b591a04a799aeceb3422cfcd1f38b49c6d1
alexgetahuntadese <alexgetahuntadese@gmail.com>
2026-06-09 05:55:28 +0300

    v 5.6

--- obj/Debug/net8.0-windows/AfranHospitalKiosk.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AfranHospitalKiosk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AfranHospitalKiosk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e980d565cc1e79d5023b4d79dde9659d97401d10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3801ad4c4f023ff9d5281a4a25e26fe92c3c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AfranHospitalKiosk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AfranHospitalKiosk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
