5b5aeb8aec3de6482452567e56a14eaac17c22b4
alexgetahuntadese <alexgetahuntadese@gmail.com>
2026-07-15 10:06:51 +0300

    Version 10.2

--- obj/Release/net8.0-windows/win-x64/AfranHospitalKiosk.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/AfranHospitalKiosk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AfranHospitalKiosk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b376d08bc8e532ef7db4339a381f557469bc0003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676ab56682367ad7b676b15f41b2e674709314e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AfranHospitalKiosk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AfranHospitalKiosk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- portable/AfranHospitalKiosk/start-server.bat
+++ portable/AfranHospitalKiosk/start-server.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d "%~dp0"
+echo Starting Afran Hospital Server Manager...
 AfranHospitalKiosk.exe server
